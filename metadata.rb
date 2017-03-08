af46ebabaf7e0528c456a843c6eeb4eeac656e93
Jeff Byrnes <thejeffbyrnes@gmail.com>
2017-03-08 14:31:52 -0500

    Add source & issues URL metadata
    Handy for Supermarket

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,9 @@ description      'Installs/Configures s3_put'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.2'
 
+source_url 'https://github.com/evertrue/s3_put-cookbook' if respond_to?(:source_url)
+issues_url 'https://github.com/evertrue/s3_put-cookbook/issues' if respond_to?(:issues_url)
+
 supports 'ubuntu', '>= 12.04'
 
 chef_version '>= 12.5'
