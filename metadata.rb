605d9bc6c047919919c25577e99d230418640000
Eric Herot <eric.github@herot.com>
2017-03-08 11:56:46 -0500

    Modernize the cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
 name             's3_put'
 maintainer       'EverTrue, Inc.'
-maintainer_email 'eric.herot@evertrue.com'
-license          'All rights reserved'
+maintainer_email 'devops@evertrue.com'
+license          'Apache v2.0'
 description      'Installs/Configures s3_put'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.1'
 
+supports 'ubuntu', '>= 12.04'
--- providers/default.rb
+++ providers/default.rb
@@ -1,20 +1,16 @@
-use_inline_resources
-
 action :upload do
-  if new_resource.remote_path.empty?
-    raise 'remote_path required for upload'
-  end
+  fail 'remote_path required for upload' if new_resource.remote_path.empty?
 
   chef_gem 'aws-s3'
 
   require 'aws/s3'
 
   AWS::S3::Base.establish_connection!(
-    :access_key_id => new_resource.access_key_id,
-    :secret_access_key => new_resource.secret_access_key
+    access_key_id: new_resource.access_key_id,
+    secret_access_key: new_resource.secret_access_key
   )
 
-  file_name = ::File.basename(new_resource.source_file)
+  file_name = ::File.basename new_resource.source_file
 
   AWS::S3::S3Object.store(
     file_name,
@@ -25,8 +21,8 @@ end
 
 action :delete do
   AWS::S3::Base.establish_connection!(
-    :access_key_id => new_resource.access_key_id,
-    :secret_access_key => new_resource.secret_access_key
+    access_key_id: new_resource.access_key_id,
+    secret_access_key: new_resource.secret_access_key
   )
 
   AWS::S3::S3Object.delete(
