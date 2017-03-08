4a292bd8c558868b7bb84ac811120cefa29b53c6
Jeff Byrnes <thejeffbyrnes@gmail.com>
2017-03-08 14:54:18 -0500

    Refactor into Chef 12.5 custom resource

--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,5 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.2'
 
 supports 'ubuntu', '>= 12.04'
+
+chef_version '>= 12.5'
--- resources/default.rb
+++ resources/default.rb
@@ -1,8 +1,41 @@
 actions :upload
 default_action :upload
 
-attribute :source_file,       kind_of: String, name_attribute: true
-attribute :bucket,            kind_of: String, required: true
-attribute :remote_path,       kind_of: String, required: true
-attribute :access_key_id,     kind_of: String, required: true
-attribute :secret_access_key, kind_of: String, required: true
+property :source_file,       String, name_attribute: true
+property :bucket,            String, required: true
+property :remote_path,       String, required: true
+property :access_key_id,     String, required: true
+property :secret_access_key, String, required: true
+
+action :upload do
+  fail 'remote_path required for upload' if remote_path.empty?
+
+  chef_gem 'aws-s3'
+
+  require 'aws/s3'
+
+  AWS::S3::Base.establish_connection!(
+    access_key_id:     access_key_id,
+    secret_access_key: secret_access_key
+  )
+
+  file_name = ::File.basename source_file
+
+  AWS::S3::S3Object.store(
+    file_name,
+    open(source_file),
+    bucket + remote_path
+  )
+end
+
+action :delete do
+  AWS::S3::Base.establish_connection!(
+    access_key_id:     access_key_id,
+    secret_access_key: secret_access_key
+  )
+
+  AWS::S3::S3Object.delete(
+    source_file,
+    bucket + remote_path
+  )
+end
