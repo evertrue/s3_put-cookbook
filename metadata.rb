04c47f756e274f24968daf2c98ae97080923c303
Eric Herot <eric.github@herot.com>
2017-03-08 11:57:10 -0500

    Version bump 1.0.2

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'devops@evertrue.com'
 license          'Apache v2.0'
 description      'Installs/Configures s3_put'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.1'
+version          '1.0.2'
 
 supports 'ubuntu', '>= 12.04'
