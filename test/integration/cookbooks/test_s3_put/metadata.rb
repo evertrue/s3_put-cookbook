05c099996a3db6316dc113735424026bc7458b9f
Jeff Byrnes <thejeffbyrnes@gmail.com>
2017-05-01 11:02:21 -0400

    Add test cookbook & config to use it
    This tests uploading & deleting a file in S3, using attribute-specified
    bucket and path (so they can be overridden in a local Kitchen config)

---
+++ test/integration/cookbooks/test_s3_put/metadata.rb
@@ -0,0 +1,8 @@
+name             'test_s3_put'
+maintainer       'EverTrue, Inc.'
+maintainer_email 'devops@evertrue.com'
+license          'Apache v2.0'
+description      'Tests s3_put'
+version          '0.1.0'
+
+depends 's3_put'
---
+++ test/integration/cookbooks/test_s3_put/recipes/default.rb
@@ -0,0 +1,15 @@
+file 'test' do
+  content 'This is a test of the s3_put Chef cookbook'
+end
+
+s3_put 'test' do
+  bucket            node['test_s3_put']['bucket']
+  remote_path       node['test_s3_put']['remote_path']
+  action            :upload
+end
+
+s3_put 'delete_test' do
+  bucket            node['test_s3_put']['bucket']
+  remote_path       node['test_s3_put']['remote_path']
+  action            :delete
+end
