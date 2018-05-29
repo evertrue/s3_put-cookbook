c46bc63b850c4340d5f85c357018b334335a2233
Jeff Byrnes <thejeffbyrnes@gmail.com>
2018-05-29 12:39:00 -0400

    Drop testing on Ubuntu 14.04, add 18.04

--- metadata.rb
+++ metadata.rb
@@ -9,7 +9,7 @@ version          '2.0.0'
 source_url 'https://github.com/evertrue/s3_put-cookbook' if respond_to?(:source_url)
 issues_url 'https://github.com/evertrue/s3_put-cookbook/issues' if respond_to?(:issues_url)
 
-supports 'ubuntu', '>= 14.04'
+supports 'ubuntu', '>= 16.04'
 
 chef_version '>= 12.8'
 
